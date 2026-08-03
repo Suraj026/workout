ea973ca8e1e7bafe6487f36286ae6330ba4fbf9a
Suraj <suraj.kumar161017@gmail.com>
2026-08-03 23:57:19 +0530

    added routes to app

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import router from "./routes/route.js";
 
 // Load environment variables
 dotenv.config();
@@ -11,11 +12,7 @@ await connectDB();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send({
-    status: "success",
-  });
-});
+app.use("/", router);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
